52c274b4045ca5001aa1c8ecd8b7d42c98994534
Grace Shea <grace.m.shea@gmail.com>
2019-05-14 14:37:48 -0700

    added validates to model and validation tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -3,7 +3,46 @@ require "test_helper"
 describe Customer do
   let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
-  end
+  describe 'validations' do
+    let(:customer) { customers():customer_one) }
+
+    it "must be valid" do
+      value(customer).must_be :valid?
+    end
+
+    it "must have a name" do
+      customer.name = nil
+      customer.valid?.must_equal false
+    end
+
+    it "must have a address" do
+      customer.phone = nil
+      customer.valid?.must_equal false
+    end
+
+    it "must have a city" do
+      customer.phone = nil
+      customer.valid?.must_equal false
+    end
+
+    it "must have a state" do
+      customer.phone = nil
+      customer.valid?.must_equal false
+    end
+
+    it "must have a postal_code" do
+      customer.phone = nil
+      customer.valid?.must_equal false
+    end
+
+    it "must have a phone" do
+      customer.phone = nil
+      customer.valid?.must_equal false
+    end
+
+    it "must have a registered date" do
+      customer.registered_ate = nil
+      customer.valid?.must_equal false
+    end
+
 end
