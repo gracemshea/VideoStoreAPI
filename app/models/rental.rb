f909c1fcf876ea907b7b534fade1c3a451299c30
Grace Shea <grace.m.shea@gmail.com>
2019-05-14 16:01:19 -0700

    rental model custom method

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,16 @@
 class Rental < ApplicationRecord
   belongs_to :movie
   belongs_to :customer
+
+  def movie_avail?(movie_id)
+    movie = Movie.find_by(id: movie_id)
+
+    if movie.nil?
+      return false
+    elsif movie.inventory > 0
+      return true
+    else
+      return false
+    end
+  end
 end
