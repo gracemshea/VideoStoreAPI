a20606fa31667dab380903de179a7868d09c91ec
Mina Shin <minamishin@hotmail.com>
2019-05-15 13:43:44 -0700

    customer controllers test

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,9 +1,35 @@
 require "test_helper"
 
 describe CustomersController do
-  it "should get index" do
-    get customers_index_url
-    value(response).must_be :success?
-  end
+  let(:customer) { customers(:one) }
+  describe "index" do
+    it "should get index" do
+      get customers_path
 
+      expect(response.header["Content-Type"]).must_include "json"
+      must_respond_with :success
+    end
+
+    it "returns an Array with all customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Array
+      expect(body.length).must_equal Customer.count
+    end
+
+    it "returns customers with exactly the required fields" do
+      keys = %w(address city name phone postal_code registered_at state)
+
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      body.each do |customer|
+        expect(customer.keys.sort).must_equal keys
+        expect(customer.keys.length).must_equal keys.length
+      end
+    end
+  end
 end
