52d654c90f90616c85caa89f89bfd463f584a0ea
Mina Shin <minamishin@hotmail.com>
2019-05-16 09:50:32 -0700

    customer controllers tests passing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -20,7 +20,7 @@ describe CustomersController do
     end
 
     it "returns customers with exactly the required fields" do
-      keys = %w(id name phone postal_code registered_at)
+      keys = %w(id movies_checked_out_count name phone postal_code registered_at)
 
       get customers_path
 
